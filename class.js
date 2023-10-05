7136a3fa4804fb4ae6aa884df4dd41d606cfc68e
Dandi Irwanto <dandi.ir20@gmail.com>
2023-10-05 21:51:20 +0700

    fix author

--- class.js
+++ class.js
@@ -1,31 +1,3 @@
-// class ContohClass {
-//     constructor(name, address) {
-//         this.name = name
-//         this.address = address
-//     }
-// }
-
-// let person1 = new ContohClass('Dandi', 'Jalan Melayu')
-
-// console.log(person1)
-// console.log(typeof person1)
-
-// class Toyota {
-//     constructor(jenisMesin, roda, codeRakitan, tonase) {
-//         this.jenisMesin = jenisMesin
-//         this.roda = roda
-//         this.codeRakitan = codeRakitan
-//         this.tonase = tonase
-//     }
-
-//     print() {
-//         console.log(`Jenis Mesin ${this.jenisMesin} roda ${this.roda} code rakitan ${this.codeRakitan} tonase ${this.tonase}`)
-//     }
-
-// }
-// let toyota = new Toyota('Honda', 4, '1231231', '3453')
-// toyota.print()
-
 class Cat {
     // add static property
     static bosCat = 'Dand'
@@ -61,3 +33,32 @@ console.log(bobby instanceof Cat)
 bobby.introduce()
 bobby.food('Fish')
 Cat.feel('Lazy')
+
+
+// class ContohClass {
+//     constructor(name, address) {
+//         this.name = name
+//         this.address = address
+//     }
+// }
+
+// let person1 = new ContohClass('Dandi', 'Jalan Melayu')
+
+// console.log(person1)
+// console.log(typeof person1)
+
+// class Toyota {
+//     constructor(jenisMesin, roda, codeRakitan, tonase) {
+//         this.jenisMesin = jenisMesin
+//         this.roda = roda
+//         this.codeRakitan = codeRakitan
+//         this.tonase = tonase
+//     }
+
+//     print() {
+//         console.log(`Jenis Mesin ${this.jenisMesin} roda ${this.roda} code rakitan ${this.codeRakitan} tonase ${this.tonase}`)
+//     }
+
+// }
+// let toyota = new Toyota('Honda', 4, '1231231', '3453')
+// toyota.print()
